06d7192aac50753ec36b81c6cd61d85eece6e848
Nikita Efremov <heknt90@mail.ru>
2021-11-21 14:12:22 +0300

    добавлен скрипт на плавную прокрутку по якорю, исправлен цвет текста в кнопке button-top

--- js/main.js
+++ js/main.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
       navbar$.removeClass("navbar_fixed");
     }
   });
+
+  // Плавная прокрутка
+  $('[data-scroll="smooth"').on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
 });
