0dd13e17c48c5e1613093991762b99ed2f52d7fb
Nikita Efremov <heknt90@mail.ru>
2021-11-21 15:42:54 +0300

    исправлен скрипт для отображения фиксированного меню при перезагрузке странице с измененной начальной позицией

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
   // Фиксированное меню
   var navbar$ = $("#navbar"),
-    firstBlock$ = $("#first-section");
-  startFixedPosition = firstBlock$.scrollTop() + firstBlock$.outerHeight();
+    firstBlock$ = $("#first-section"),
+    startFixedPosition = firstBlock$.scrollTop() + firstBlock$.outerHeight(),
+    menuVisibleClassName = "navbar_fixed";
 
-  $(document).scroll(function () {
-    if (window.pageYOffset >= startFixedPosition) {
-      navbar$.addClass("navbar_fixed");
-    } else {
-      navbar$.removeClass("navbar_fixed");
+  if (navbar$ && firstBlock$) {
+    console.log(1, isNeededYOffset());
+    if (isNeededYOffset()) {
+      showMenu(navbar$);
     }
-  });
+
+    $(document).scroll(function () {
+      if (isNeededYOffset()) {
+        showMenu(navbar$);
+      } else {
+        navbar$.removeClass(menuVisibleClassName);
+      }
+    });
+  } else {
+    console.log(navbar$, firstBlock$);
+  }
+
+  function showMenu(menu) {
+    menu.addClass(menuVisibleClassName);
+  }
+
+  function isNeededYOffset() {
+    return window.pageYOffset >= startFixedPosition;
+  }
 
   // Плавная прокрутка
   $('[data-scroll="smooth"').on("click", function (event) {
